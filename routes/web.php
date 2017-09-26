9a5093dcece53cc8c86e4e898029040c8e286e4c
nszess <nszess@gmail.com>
2017-09-25 22:52:04 -0400

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'IndexController@index_get');
+Route::get('/about', 'AboutController@about_get');
+Route::get('/calculator/{dis?}/{time?}/{temp?}/{ph?}/{logGiardia?}/{calcMethod?}', 'CalcController@calculator_get');
+Route::post('/calculator', 'CalcController@calculator_post');
