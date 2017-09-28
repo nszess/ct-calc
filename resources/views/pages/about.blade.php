633c6aa3219f8ed1a4ba7b20c6dcc6f17faabba1
nszess <nszess@gmail.com>
2017-09-28 11:59:18 -0400

    Added pdf link to manual on about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,4 +6,7 @@
 <p>This Contact Time Calculator calculates the contact time provided and the amount of contact time needed to provide adequate inactivation of Giardia and viruses for a range of disinfectants.  These calculations can be useful for determining compliance with the Surface Water Treatment Rule.</p>
 <p>The contact time provided is calculated based on user inputs of the disinfectant residual and the time provided.</p>
 <p>The amount contact time needed is determined from user inputs of the type of disinfectant, the logs of inactivation needed, temperature, pH, and disinfectant residual.  The calculator uses the methods and tables in EPA Manual EPA815-R-99-013, titled “Disinfection Profiling and Benchmarking Guidance Manual”, dated August 1999, for determining the amount of inactivation needed.</p>
+
+<a href='{{URL::asset('Manual.pdf')}}'>Manual t</a>
+
 @endsection
